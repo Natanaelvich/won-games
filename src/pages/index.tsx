b93b427d21e9317fd1076163c8522e7639b44143
Natanael Lima <taelima1997@gmail.com>
2021-03-27 00:29:19 -0300

    Connect mock data to the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,24 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import Home from 'templates/Home'
+import Home, { HomeTemplateProps } from 'templates/Home'
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
-export default function Index(props: any) {
+export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-// ATENÇÃO:
-// os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
-
-// getStaticProps => gerar estático em build time
-// getServerSideProps => gerar via ssr a cada request
-// getInitialProps => gerar via ssr a cada request
 export function getServerSideProps() {
-  // faz lógica
-  // pode ser buscar dados numa API
-  // fazer calculo|leitura de context
-
-  // retorno dos dados
   return {
     props: {
-      heading: 'Olha eu aqui!!!!! 1234 ssr'
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcommingGames: gamesMock,
+      upcommingHighligth: highlightMock,
+      upcommingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighligth: highlightMock
     }
   }
 }
