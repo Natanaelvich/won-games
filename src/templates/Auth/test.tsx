a40998c66561a5804d56b85ce519947c31602771
Natanael Lima <taelima1997@gmail.com>
2021-03-30 21:07:16 -0300

    Add Auth initial structure

---
+++ src/templates/Auth/test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import Auth from '.'
+
+describe('<Auth />', () => {
+  it('should render all components and children', () => {
+    render(
+      <Auth title="Auth Title">
+        <input type="text" />
+      </Auth>
+    )
+
+    // verifiquem se tem 2 logos
+
+    // verifica se tem o heading principal do banner
+
+    // verifica se tem o subtitle
+
+    // verifica se tem o title do content
+
+    // verifica se o children tá sendo renderizado
+  })
+})
